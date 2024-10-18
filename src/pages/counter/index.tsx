5f3718389ce25204e580e9d611ea842a0b79442a
wongyichen <wongyichen@163.com>
2024-10-18 19:28:23 +0800

    update

--- src/pages/counter/index.tsx
+++ src/pages/counter/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useEffectOnActive, useKeepAliveContext } from '../../components/KeepAliveProvider';
 import { DatePicker, Select } from 'antd';
 
@@ -6,6 +6,7 @@ function Counter() {
     const [count, setCount] = useState(0);
 
     const { destroy } = useKeepAliveContext();
+    const domRef = useRef<HTMLDivElement>(null);
 
     useEffectOnActive(
         active => {
@@ -18,8 +19,16 @@ function Counter() {
         [count],
     );
 
+    useEffect(() => {
+        // log height and width of the div
+        const dom = domRef.current;
+        if (dom) {
+            console.log(`div height: ${dom.clientHeight} width: ${dom.clientWidth}`);
+        }
+    }, []);
+
     return (
-        <div>
+        <div ref={domRef}>
             <p>Count: {count}</p>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <input type="text" />
